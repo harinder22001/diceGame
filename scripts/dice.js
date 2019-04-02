1dee3c63dfaca0fcb5ed0d62ed1211d7a1e5d711
harinder22001 <singh.p.harinder@gmail.com>
2019-04-01 21:30:23 -0500

    Add files via upload

---
+++ scripts/dice.js
@@ -0,0 +1,24 @@
+var num1 = Math.floor((Math.random()) * 6 +1);
+var num2 = Math.floor((Math.random()) * 6 +1);
+
+
+
+document.querySelectorAll(".dice")[0].querySelector("img").setAttribute("src", "images/dice" + num1 + ".png") ;
+
+
+document.querySelectorAll(".dice")[1].querySelector("img").setAttribute("src","images/dice" + num2 + ".png");
+
+if(num1>num2){
+
+document.querySelector("h1").textContent="⛳ Player 1 Wins";
+}
+else if (true) {
+
+  document.querySelector("h1").textContent="Player 2 Wins ⛳" ;
+
+
+
+}else{
+  document.querySelector("h1").textContent="Its a tie";
+
+}
